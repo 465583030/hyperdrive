a4f1d0879fb291ee42ebc446029b0a1a9fe4419c
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-05-24 09:00:48 +1000

    Refactored.

--- router/router.go
+++ router/router.go
@@ -22,25 +22,23 @@ func (r *Router) CreateSnapshot() ([]byte, error) {
 	return []byte{}, nil
 }
 
-// HandleRoute dispatches the incoming request to the appropriate route.
-func (r *Router) HandleRoute(res http.ResponseWriter, req *http.Request) {
+func (r *Router) handleRoute(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("hello\n"))
 }
 
-// HandleAPIRoute is the entrypoint for route management api
-func (r *Router) HandleAPIRoute(res http.ResponseWriter, req *http.Request) {
+func (r *Router) handleAPIRoute(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 }
 
 func (r *Router) startRouterService(port int) {
 	routerService := http.NewServeMux()
-	routerService.HandleFunc("/", r.HandleRoute)
+	routerService.HandleFunc("/", r.handleRoute)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), routerService)
 }
 
 func (r *Router) startAPIService(port int) {
 	apiService := http.NewServeMux()
-	apiService.HandleFunc("/routes", r.HandleAPIRoute)
+	apiService.HandleFunc("/routes", r.handleAPIRoute)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), apiService)
 }
 
