f016b60872df1a96f62a2259a49cbed5a2b3f1b1
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2017-05-24 19:42:12 +1000

    Added the support for api-port argument.

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ fi
 
 pushd build
 
-./hyperdrive -cluster "http://127.0.0.1:8211,http://127.0.0.1:8212,http://127.0.0.1:8213" -id $ID -port "831${ID}"
+./hyperdrive -cluster "http://127.0.0.1:8211,http://127.0.0.1:8212,http://127.0.0.1:8213" -id $ID -port "831${ID}" -api-port "841${ID}"
 
 popd
